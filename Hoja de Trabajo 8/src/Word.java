dac3a7944ae42ea44990e2117a324a62ed43e74b
MarvinJhonjairo <marvinjh_94@hotmail.com>
2014-10-09 15:00:25 -0600

    Update Word.java

--- Hoja de Trabajo 8/src/Word.java
+++ Hoja de Trabajo 8/src/Word.java
@@ -4,53 +4,46 @@ Algoritmos y Estructuras de Datos - 2011
 Hoja de trabajo 7 
 Autor: Eduardo Castellanos
 
-Descripción: Word. Clase para almacenar las palabras junto con su tipo. 
+DescripciÃ³n: Word. Clase para almacenar las palabras junto con su tipo. 
 */
+
 class Word implements Comparable<Word> {
-	private String word;
-	private String type;
-	
-	// Constructor, inicializa la palabra con su tipo
-	public Word(String word, String type)
-	{
-		this.word=word;
-		this.type=type;
-	}
-	
-	public Word()
-	{
-		this.word= "";
-		this.type="";
-	}
-	
-	// Comparadores, solo importa comparar la palabra, no el tipo.
-	public int compareTo(Word o)
-	{
-		return this.word.compareTo(o.getWord());
-	}
-	
-	public boolean equals(Object obj)
-	{
-		return (obj instanceof Word && getWord().equals(((Word)obj).getWord()));
-	}
-	
-	// Métodos de acceso..
-	public void setWord(String word)
-	{
-		this.word=word;
-	}
-	public void setType(String type)
-	{
-		this.type=type;
-	}
-	public String getWord()
-	{
-		return word;
-	}
-	
-	public String getType()
-	{
-		return type;
-	}
-	
+        private String word;
+        private String type;
+       
+        // Constructor, inicializa la palabra con su tipo
+        public Word(String word, String type){
+                this.word=word;
+                this.type=type;
+        }
+       
+        public Word(){
+                this.word= "";
+                this.type="";
+        }
+       
+        // Comparadores, solo importa comparar la palabra, no el tipo.
+        public int compareTo(Word o){
+                return this.word.compareTo(o.getWord());
+        }
+       
+        public boolean equals(Object obj){
+                return (obj instanceof Word && getWord().equals(((Word)obj).getWord()));
+        }
+       
+        // MÃ©todos de acceso..
+        public void setWord(String word){
+                this.word=word;
+        }
+        public void setType(String type){
+                this.type=type;
+        }
+        public String getWord(){
+                return word;
+        }
+       
+        public String getType(){
+                return type;
+        }
+       
 }
