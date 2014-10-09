b2049d848d650cfc3ea1e1ac89a2dbc523cf4279
MarvinJhonjairo <marvinjh_94@hotmail.com>
2014-10-09 15:00:52 -0600

    Update WordSet.java

--- Hoja de Trabajo 8/src/WordSet.java
+++ Hoja de Trabajo 8/src/WordSet.java
@@ -4,11 +4,9 @@ Algoritmos y Estructuras de Datos - 2011
 Hoja de trabajo 7 
 Autor: Eduardo Castellanos
 
-DescripciÛn: WordSet. Interfaz para las implementaciones de clases que almacenar·n palabras.
+Descripci√≥n: WordSet. Interfaz para las implementaciones de clases que almacenar√°n palabras.
 */
 public interface WordSet {
-
-	public void add(Word wordObject);
-	public Word get(Word word);
-	
+        public void add(Word wordObject);
+        public Word get(Word word);     
 }
